c3db1559f57bf2ee01b996408261f11b17daa781
tlwaller <tlwaller99@gmail.com>
2019-12-17 17:27:41 -0600

    started on register page

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import birb from "../images/white-birb.png";
 
 class Register extends Component {
   render() {
-    return <div>register page</div>;
+    return (
+      <div id="reg-container">
+        <div id="reg-box">
+          <header>
+            <img src={birb} alt="birb" />
+            <button className="reg-next-btn">Next</button>
+          </header>
+        </div>
+      </div>
+    );
   }
 }
 
