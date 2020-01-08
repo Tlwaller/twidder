164e89d95c9b929fe0ba0acdc687268eb1c1e9cd
tlwaller <tlwaller99@gmail.com>
2020-01-08 17:24:24 -0600

    created inputs for register form

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -7,9 +7,21 @@ class Register extends Component {
       <div id="reg-container">
         <div id="reg-box">
           <header>
+            <div id="reg-head-spacer" />
             <img src={birb} alt="birb" />
-            <button className="reg-next-btn">Next</button>
+            <button className="reg-btn">Register</button>
           </header>
+
+          <form>
+            <h1 id="reg-title">Put your stuff in</h1>
+            <label className="reg-label">
+              Username <input className="reg-input" />
+            </label>
+
+            <label className="reg-label">
+              Password <input className="reg-input" />
+            </label>
+          </form>
         </div>
       </div>
     );
