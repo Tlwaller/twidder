ff6ffa28fcc3bfaa736a9c21b4f88dfa043b3fc3
tlwaller <tlwaller99@gmail.com>
2020-01-09 18:43:44 -0600

    added dropdown menu for DOB

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -12,7 +12,7 @@ class Register extends Component {
             <button className="reg-btn">Register</button>
           </header>
 
-          <form>
+          <form className="reg-form">
             <h1 id="reg-title">Put your stuff in</h1>
             <label className="reg-label">
               Username <input className="reg-input" />
@@ -20,6 +20,27 @@ class Register extends Component {
 
             <label className="reg-label">
               Password <input className="reg-input" />
+            </label>
+          </form>
+
+          <div id="reg-date-desc">
+            <h3>Your birthday</h3>
+            <p>So we know you're old enough for grown up stuff.</p>
+          </div>
+          <form className="reg-form" id="reg-date-form">
+            <label className="reg-label" id="reg-month-drop">
+              Month
+              <select className="reg-dropdown">
+                <option>January</option>
+              </select>
+            </label>
+            <label className="reg-label" id="reg-day-drop">
+              Day
+              <select className="reg-dropdown"></select>
+            </label>
+            <label className="reg-label" id="reg-year-drop">
+              Year
+              <select className="reg-dropdown"></select>
             </label>
           </form>
         </div>
