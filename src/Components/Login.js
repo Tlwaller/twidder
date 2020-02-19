f1b5efd385d130695312c17b6059e7bcabac4124
Trey Waller <Trey@Treys-MBP.attlocal.net>
2020-02-19 14:05:59 -0600

    login front end created

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import birb from '../images/white-birb.png';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   render() {
-    return <div>Login Page</div>;
+    return (
+    <div id='login-bg'>
+      <div id='login-container'>
+        <img src={birb} alt='birb' style={{height: 30 + 'px'}, {width: 30 + 'px'}}/>
+        <h2>Log in to Twidder!</h2>
+
+        <form id='login-form'>
+          <label className='login-label'>Username
+            <input className='login-input' type='text' name='username'/>
+          </label>
+          <label className='login-label'>Password
+            <input className='login-input' type='password' name='password'/>
+          </label>
+          <button id='login-btn' >Log in</button>
+        </form>
+
+        <Link id='register-link' to='/register'>Sign up for Twidder</Link>
+      </div>
+    </div>
+    )
   }
 }
 
